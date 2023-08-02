fe18f2f39dd67ca36b26a6574267a1dea8aae45a
Joe Meers Jankowski <joe@Joes-MacBook-Pro-2.local>
2023-08-02 13:00:29 -0700

    first commit for babc blog

--- components/hero-post.js
+++ components/hero-post.js
@@ -1,20 +1,20 @@
-import Link from 'next/link'
-import Avatar from '../components/avatar'
-import DateComponent from '../components/date'
-import CoverImage from '../components/cover-image'
+import Link from "next/link";
+import Avatar from "../components/avatar";
+import DateComponent from "../components/date";
+import CoverImage from "../components/cover-image";
 
 export default function HeroPost({
   title,
-  coverImage,
-  date,
-  excerpt,
-  author,
+  // coverImage,
+  // date,
+  // excerpt,
+  // author,
   slug,
 }) {
   return (
     <section>
       <div className="mb-8 md:mb-16">
-        <CoverImage title={title} slug={slug} url={coverImage.url} />
+        {/* <CoverImage title={title} slug={slug} url={coverImage.url} /> */}
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
@@ -24,14 +24,14 @@ export default function HeroPost({
             </Link>
           </h3>
           <div className="mb-4 md:mb-0 text-lg">
-            <DateComponent dateString={date} />
+            {/* <DateComponent dateString={date} /> */}
           </div>
         </div>
         <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          {author && <Avatar name={author.name} picture={author.picture} />}
+          {/* <p className="text-lg leading-relaxed mb-4">{excerpt}</p> */}
+          {/* {author && <Avatar name={author.name} picture={author.picture} />} */}
         </div>
       </div>
     </section>
-  )
+  );
 }
