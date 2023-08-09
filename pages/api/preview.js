fa0b163c0c6a0cfd80e7094113999c40772f8968
Joe Meers Jankowski <joe@Joes-MacBook-Pro-2.local>
2023-08-08 17:06:32 -0700

    updated preview.js and added preview brewery function to api for previewing on contentful

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,33 +1,32 @@
-import { getPreviewPostBySlug } from '../../lib/api'
+import { getPreviewBreweryBySlug } from "../../lib/api";
 
 export default async function preview(req, res) {
-  const { secret, slug } = req.query
+  const { secret, slug } = req.query;
 
   if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !slug) {
-    return res.status(401).json({ message: 'Invalid token' })
+    return res.status(401).json({ message: "Invalid token" });
   }
 
   // Fetch the headless CMS to check if the provided `slug` exists
-  const post = await getPreviewPostBySlug(slug)
+  const brewery = await getPreviewBreweryBySlug(slug);
 
   // If the slug doesn't exist prevent preview mode from being enabled
-  if (!post) {
-    return res.status(401).json({ message: 'Invalid slug' })
+  if (!brewery) {
+    return res.status(401).json({ message: "Invalid slug" });
   }
 
   // Enable Draft Mode by setting the cookie
-  res.setDraftMode({ enable: true })
+  res.setDraftMode({ enable: true });
 
-  // Redirect to the path from the fetched post
+  // Redirect to the path from the fetched brewery
   // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  // res.writeHead(307, { Location: `/posts/${post.slug}` })
-  const url = `/posts/${post.slug}`
-  res.setHeader('Content-Type', 'text/html')
+  const url = `/breweries/${brewery.slug}`;
+  res.setHeader("Content-Type", "text/html");
   res.write(
     `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${url}" />
     <script>window.location.href = '${url}'</script>
     </head>
     </html>`
-  )
-  res.end()
+  );
+  res.end();
 }
